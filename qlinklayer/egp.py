c5e09f39696480744ec034debd8fe5682a166ca4
Axel Dahlberg <axel.dahlberg12@gmail.com>
2018-08-02 16:54:49 +0200

    Fixed bug with _return_ok during timeout

--- qlinklayer/egp.py
+++ qlinklayer/egp.py
@@ -749,11 +749,6 @@ class NodeCentricEGP(EGP):
         """
         logger.debug("Returning okay")
 
-        # Make the communication qubit available for subsequent attempts
-        comm_q, storage_q = self.scheduler.curr_gen[2:4]
-        if comm_q != storage_q:
-            self.qmm.free_qubit(comm_q)
-
         # Get the current request
         creq = self.scheduler.get_request(aid=aid)
         now = self.get_current_time()
@@ -761,6 +756,11 @@ class NodeCentricEGP(EGP):
         if creq is None:
             logger.error("Request not found!")
             self.issue_err(err=self.ERR_OTHER)
+
+        # Make the communication qubit available for subsequent attempts
+        comm_q, storage_q = self.scheduler.curr_gen[2:4]
+        if comm_q != storage_q:
+            self.qmm.free_qubit(comm_q)
 
         # Get the fidelity estimate from FEU
         logger.debug("Estimating fidelity")
