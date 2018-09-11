d80a39329f7f1c20a2153fd2837ea54b53eb8903
Axel Dahlberg <axel.dahlberg12@gmail.com>
2018-09-11 17:57:00 +0200

    Improved post processing functionlaity

--- simulations/create_measure_simulation/readonly/archivejob.sh
+++ simulations/create_measure_simulation/readonly/archivejob.sh
@@ -11,6 +11,8 @@ JOB_ID=$1
 TMP_DIR=$2
 RESULT_DIR=$3
 POST_PROC=$4
+timestamp=$5
+paramsetfile=$6
 
 # Concatenate output CSV files and move to project space
 mkdir -p $RESULT_DIR
@@ -20,13 +22,16 @@ mv $TMP_DIR/* $RESULT_DIR
 # Move the slurm file
 mv "${SIMULATION_DIR}/slurm-${JOB_ID}.out" $RESULT_DIR
 
-# Zip the results directory
-zip -r "${RESULT_DIR}.zip" $RESULT_DIR
 
 # Cleanup
 rmdir $TMP_DIR
 
 # Run post-processing
 if [ "$POST_PROC" == "y" ]; then
-    sbatch --out="${RESULT_DIR}/post_proc_output.out" "${SIMULATION_DIR}/setupsim/post_processing.sh" $RESULT_DIR
+    outfile="${RESULT_DIR}/post_proc_output.out"
+    post_proc_file="${SIMULATION_DIR}/setupsim/post_processing.sh"
+    sbatch $post_proc_file $RESULT_DIR $timestamp $paramsetfile
+else
+    # Zip the results directory
+    zip -r "${RESULT_DIR}.zip" $RESULT_DIR
 fi
--- simulations/create_measure_simulation/setupsim/post_processing.sh
+++ simulations/create_measure_simulation/setupsim/post_processing.sh
@@ -3,6 +3,51 @@
 #SBATCH -t 1:00:00
 
 RESULT_DIR=$1
+timestamp=$2
+paramsetfile=$3
 
-# Run post processing
-python3 "${SIMULATION_DIR}/../analysis_sql_data.py" --results-path $RESULT_DIR --no-plot
+########################
+# Setup stopos pool
+# to run post processing
+# in parallell.
+########################
+
+export STOPOS_POOL=pool_post_proc
+stopos create
+stopos add $paramsetfile
+
+processes=`sara-get-num-cores`
+
+for ((i=1; i<=processes; i++)); do
+(
+    while true; do
+        # Move to the first item in the pool
+        stopos next &> /dev/null
+
+        # Check if there are more items in the pool
+        if [ "$STOPOS_RC" != "OK" ]; then
+            break
+        fi
+
+        # Get the next parameters from the pool
+        params=( $STOPOS_VALUE )
+
+        # Extract the parameters
+        actual_key=${params[0]} # Key to parameter set in paramcombinations-file
+        runindex=${params[1]} # Run index
+
+        # Results file (without file-type)
+        result_file="${RESULT_DIR}/${timestamp}_key_${actual_key}_run_${runindex}"
+
+        # Run post processing
+        analysis_file="${SIMULATION_DIR}/../analysis_sql_data.py"
+        python3 $analysis_file --results-path "${results_file}.db" --no-plot --save-figs --save-output --analysis-folder "${result_file}_analysis"
+
+        # Remove the parameters from the pool
+        stopos remove -p $STOPOS_POOL
+) &
+done
+wait
+
+# Zip the results directory
+zip -r "${RESULT_DIR}.zip" $RESULT_DIR
